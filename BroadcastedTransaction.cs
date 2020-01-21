253ef874a066b4f62add05c49b0d9ad53aa49ff9
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 01:27:55 +0200

    Naming fix

---
+++ BroadcastedTransaction.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Numerics;
+
+namespace CrappyPrizm
+{
+    internal class BroadcastedTransaction
+    {
+        #region Var
+        [JsonProperty("transaction")]
+        public BigInteger Id { get; }
+        
+        [JsonProperty("fullHash")]
+        public string Hash { get; }
+        #endregion
+
+        #region Init
+        [JsonConstructor]
+        public BroadcastedTransaction([JsonProperty("transaction")]BigInteger id, [JsonProperty("fullHash")]string hash)
+        {
+            Id = id;
+            Hash = hash;
+        }
+        #endregion
+
+        #region Functions
+        public override string ToString() => Hash;
+        public override int GetHashCode() => Id.GetHashCode();
+        public override bool Equals(object? obj) => obj is BroadcastedTransaction transaction && transaction.Id == Id;
+        #endregion
+    }
+}
