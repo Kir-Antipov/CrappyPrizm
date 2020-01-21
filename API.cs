ab6ce68f9ef345af134fea0b8ff136b72a5dfd1c
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 00:56:34 +0200

    Made base functions for sending requests to API

---
+++ API.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CrappyPrizm
+{
+    public static class API
+    {
+        #region Var
+        private const string Url = "https://wallet.prizm.space/";
+        private static readonly HttpClient Client = new HttpClient { BaseAddress = new Uri(Url) };
+        #endregion
+
+        #region Functions
+        private static async Task<T> MakeRequestAsync<T>(string type, params (string key, string value)[] parameters)
+        {
+            HttpResponseMessage response = await MakeRequestAsync(type, parameters);
+            string json = await response.Content.ReadAsStringAsync();
+            return json.Contains("error") ? default : JsonConvert.DeserializeObject<T>(json);
+        }
+
+        private static Task<HttpResponseMessage> MakeRequestAsync(string type, params (string key, string value)[] parameters)
+        {
+            FormUrlEncodedContent content = new FormUrlEncodedContent(
+                parameters.Select(param => new KeyValuePair<string, string>(param.key, param.value))
+                .Append(new KeyValuePair<string, string>("rnd", new Random().Next(int.MinValue, 0).ToString()))
+            );
+            string requestUri = $"/prizm?requestType={type}";
+            return Client.PostAsync(requestUri, content);
+        }
+        #endregion
+    }
+}
