b82276ab776eb9c0d216161e1cde3da19bae963f
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 00:50:16 +0200

    Added class representing message

---
+++ Message.cs
@@ -0,0 +1,22 @@
+﻿namespace CrappyPrizm
+{
+    public class Message
+    {
+        #region Var
+        public string Text { get; }
+        #endregion
+
+        #region Init
+        public Message(string text) => Text = text;
+        
+        public static implicit operator string(Message message) => message.Text;
+        public static implicit operator Message(string text) => new Message(text);
+        #endregion
+
+        #region Functions
+        public override string ToString() => Text;
+        public override int GetHashCode() => Text.GetHashCode();
+        public override bool Equals(object? obj) => obj is Message message && message.Text == Text;
+        #endregion
+    }
+}
