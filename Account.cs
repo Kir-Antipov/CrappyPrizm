b08be43ff27243781076ddb9519ad977dfb80d42
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 00:47:11 +0200

    Added class representing prizm account

---
+++ Account.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace CrappyPrizm
+{
+    public class Account
+    {
+        #region Var
+        [JsonProperty("account")]
+        public long Id { get; }
+
+        [JsonProperty("accountRS")]
+        public string Address { get; }
+
+        [JsonProperty("balanceNQT")]
+        public decimal Balance { get; }
+
+        [JsonProperty("publicKey")]
+        public string PublicKey { get; }
+
+        public string? PrivateKey { get; }
+        #endregion
+
+        #region Init
+        [JsonConstructor]
+        public Account( [JsonProperty("account")]long id,
+                        [JsonProperty("accountRS")]string address,
+                        [JsonProperty("balanceNQT")]decimal balance,
+                        [JsonProperty("publicKey")]string publicKey)
+        {
+            Id = id;
+            Address = address;
+            Balance = balance;
+            PublicKey = publicKey;
+            PrivateKey = null;
+        }
+
+        public Account(long id, string address, decimal balance, string publicKey, string privateKey)
+        {
+            Id = id;
+            Address = address;
+            Balance = balance;
+            PublicKey = publicKey;
+            PrivateKey = privateKey;
+        }
+        #endregion
+
+        #region Functions
+        public Account With(string privateKey) => new Account(Id, Address, Balance, PublicKey, privateKey);
+
+        public override int GetHashCode() => Id.GetHashCode();
+        public override string ToString() => Address;
+        public override bool Equals(object? obj) => obj is Account account && account.Id == Id;
+        #endregion
+    }
+}
