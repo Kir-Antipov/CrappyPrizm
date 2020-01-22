8b5cfa8264fa0e72d895f38d8d840765f4d00c32
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 02:50:06 +0200

    Created functions for `bytes <-> hex` conversions

--- Convert.cs
+++ Convert.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Text;
-using System.Collections.Generic;
 
 namespace CrappyPrizm
 {
@@ -8,5 +7,40 @@ namespace CrappyPrizm
     {
         public static decimal CoinsToAmount(decimal coins) => coins / 100;
         public static decimal AmountToCoins(decimal amount) => amount * 100;
+
+        public static string BytesToHex(byte[] bytes)
+        {
+            const string alphabet = "0123456789abcdef";
+
+            StringBuilder hex = new StringBuilder(bytes.Length * 2);
+            for (int i = 0; i < bytes.Length; ++i)
+            {
+                byte b = bytes[i];
+                hex.Append(alphabet[b >> 4]);
+                hex.Append(alphabet[b & 0xF]);
+            }
+            return hex.ToString();
+        }
+
+        public static byte[] HexToBytes(string hex)
+        {
+            static int GetHexVal(char hex)
+            {
+                int val = hex;
+                // 58 == '9' + 1
+                // 97 == 'a'
+                // 87 == 'a' - 10
+                // 55 == 'A' - 10
+                return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+            }
+
+            if (hex.Length % 2 == 1)
+                throw new ArgumentException(nameof(hex), "Invalid length");
+
+            byte[] bytes = new byte[hex.Length >> 1];
+            for (int i = 0; i < hex.Length >> 1; ++i)
+                bytes[i] = (byte)((GetHexVal(hex[i << 1]) << 4) + (GetHexVal(hex[(i << 1) + 1])));
+            return bytes;
+        }
     }
 }
