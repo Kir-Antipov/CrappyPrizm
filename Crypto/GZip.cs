938780347083a0e199ce6824501816efedde69cd
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 09:04:58 +0200

    Cleanup

--- Crypto/GZip.cs
+++ Crypto/GZip.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.IO.Compression;
 
 namespace CrappyPrizm.Crypto
@@ -9,27 +8,9 @@ namespace CrappyPrizm.Crypto
         public static byte[] Compress(byte[] input)
         {
             using MemoryStream result = new MemoryStream();
-            byte[] lengthBytes = BitConverter.GetBytes(input.Length);
-            result.Write(lengthBytes, 0, 4);
-
             using Stream compressionStream = new GZipStream(result, CompressionMode.Compress);
             compressionStream.Write(input, 0, input.Length);
-            compressionStream.Flush();
-
             return result.ToArray();
-        }
-
-        public static byte[] Decompress(byte[] input)
-        {
-            using Stream source = new MemoryStream(input);
-            byte[] lengthBytes = new byte[4];
-            source.Read(lengthBytes, 0, 4);
-            int length = BitConverter.ToInt32(lengthBytes, 0);
-            using Stream decompressionStream = new GZipStream(source, CompressionMode.Decompress);
-            
-            byte[] result = new byte[length];
-            decompressionStream.Read(result, 0, length);
-            return result;
         }
     }
 }
