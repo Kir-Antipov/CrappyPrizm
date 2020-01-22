24066f8f9cbfb7fa336eb286179d81a61b344cd8
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 03:51:39 +0200

    Made `Block` entity

---
+++ Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace CrappyPrizm
+{
+    public class Block
+    {
+        #region Var
+        public BigInteger Id { get; }
+        public int Height { get; }
+        public int MaxHeight { get; }
+        #endregion
+
+        #region Init
+        public Block(BigInteger id, int height, int maxHeight = int.MaxValue)
+        {
+            Id = id;
+            Height = height;
+            MaxHeight = maxHeight;
+        }
+        #endregion
+
+        #region Functions
+        public override string ToString() => Id.ToString();
+        public override int GetHashCode() => Id.GetHashCode();
+        public override bool Equals(object? obj) => obj is Block block && block.Id == Id;
+        #endregion
+    }
+}
