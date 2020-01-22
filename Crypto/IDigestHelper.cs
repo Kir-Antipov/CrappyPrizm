4ff40714ba1c7d52e6523b903d822b74cb0c79b9
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 17:38:16 +0200

    BouncyCastle's implementation of SHA-256 ¯\_(ツ)_/¯

---
+++ Crypto/IDigestHelper.cs
@@ -0,0 +1,21 @@
+﻿using Org.BouncyCastle.Crypto;
+
+namespace CrappyPrizm.Crypto
+{
+    public static class IDigestHelper
+    {
+        public static void Update(this IDigest digest, byte[] input) => digest.BlockUpdate(input, 0, input.Length);
+
+        public static byte[] Digest(this IDigest digest)
+        {
+            byte[] result = new byte[digest.GetDigestSize()];
+            digest.DoFinal(result, 0);
+            return result;
+        }
+        public static byte[] Digest(this IDigest digest, byte[] input)
+        {
+            digest.Update(input);
+            return digest.Digest();
+        }
+    }
+}
