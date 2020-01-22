dbc748b6d157e1cff96b201e97a9b664b12dba44
Kir-Antipov <kp_antipov@mail.ru>
2020-01-22 07:18:39 +0200

    Added class for GZip compression/decompression

---
+++ Crypto/GZip.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace CrappyPrizm.Crypto
+{
+    internal static class GZip
+    {
+        public static byte[] Compress(byte[] input)
+        {
+            using MemoryStream result = new MemoryStream();
+            byte[] lengthBytes = BitConverter.GetBytes(input.Length);
+            result.Write(lengthBytes, 0, 4);
+
+            using Stream compressionStream = new GZipStream(result, CompressionMode.Compress);
+            compressionStream.Write(input, 0, input.Length);
+            compressionStream.Flush();
+
+            return result.ToArray();
+        }
+
+        public static byte[] Decompress(byte[] input)
+        {
+            using Stream source = new MemoryStream(input);
+            byte[] lengthBytes = new byte[4];
+            source.Read(lengthBytes, 0, 4);
+            int length = BitConverter.ToInt32(lengthBytes, 0);
+            using Stream decompressionStream = new GZipStream(source, CompressionMode.Decompress);
+            
+            byte[] result = new byte[length];
+            decompressionStream.Read(result, 0, length);
+            return result;
+        }
+    }
+}
