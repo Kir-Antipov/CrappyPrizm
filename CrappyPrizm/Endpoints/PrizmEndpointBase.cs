c5904ed8112f0d137e1a601dcf2fa46fe482f074
Kir-Antipov <kp_antipov@mail.ru>
2020-03-02 21:27:05 +0200

    Added helper methods

--- CrappyPrizm/Endpoints/PrizmEndpointBase.cs
+++ CrappyPrizm/Endpoints/PrizmEndpointBase.cs
@@ -1,4 +1,10 @@
-﻿using CrappyPrizm.API;
+﻿using System.Linq;
+using CrappyPrizm.API;
+using Newtonsoft.Json;
+using System.Net.Http;
+using CrappyPrizm.Exceptions;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using CrappyPrizm.RequestBuilders;
 
 namespace CrappyPrizm.Endpoints
@@ -15,6 +21,27 @@ namespace CrappyPrizm.Endpoints
         public PrizmEndpointBase(IApiClient apiClient) : base(apiClient, DefaultRequestBuilder, BaseEndpointUrl) { }
 
         public PrizmEndpointBase(IApiClient apiClient, IRequestBuilder requestBuilder) : base(apiClient, requestBuilder, BaseEndpointUrl) { }
+        #endregion
+
+        #region Functions
+        protected async Task<T> SendRequestAsync<T>(string requestType, HttpMethod? httpMethod = null, IEnumerable<KeyValuePair<string, string>>? content = null)
+        {
+            HttpResponseMessage response = await base.SendRequestAsync(requestType, httpMethod, content);
+            string json = await response.Content.ReadAsStringAsync();
+            return json.Contains("errorCode") ? throw JsonConvert.DeserializeObject<APIException>(json) : JsonConvert.DeserializeObject<T>(json);
+        }
+
+        protected Task<T> SendRequestAsync<T>(string requestType, params KeyValuePair<string, string>[] content) => SendRequestAsync<T>(requestType, null, (IEnumerable<KeyValuePair<string, string>>)content);
+        protected Task<T> SendRequestAsync<T>(string requestType, HttpMethod httpMethod, params KeyValuePair<string, string>[] content) => SendRequestAsync<T>(requestType, httpMethod, (IEnumerable<KeyValuePair<string, string>>)content);
+
+        protected Task<T> SendRequestAsync<T>(string requestType, params KeyValuePair<string, object?>[] content) => SendRequestAsync<T>(requestType, null, content.Select(x => new KeyValuePair<string, string>(x.Key, x.Value?.ToString() ?? string.Empty)));
+        protected Task<T> SendRequestAsync<T>(string requestType, HttpMethod httpMethod, params KeyValuePair<string, object?>[] content) => SendRequestAsync<T>(requestType, httpMethod, content.Select(x => new KeyValuePair<string, string>(x.Key, x.Value?.ToString() ?? string.Empty)));
+
+        protected Task<T> SendRequestAsync<T>(string requestType, params (string key, object? value)[] content) => SendRequestAsync<T>(requestType, null, content.Select(x => new KeyValuePair<string, string>(x.key, x.value?.ToString() ?? string.Empty)));
+        protected Task<T> SendRequestAsync<T>(string requestType, HttpMethod httpMethod, params (string key, object? value)[] content) => SendRequestAsync<T>(requestType, httpMethod, content.Select(x => new KeyValuePair<string, string>(x.key, x.value?.ToString() ?? string.Empty)));
+
+        protected Task<T> SendRequestAsync<T>(string requestType, params (string key, string value)[] content) => SendRequestAsync<T>(requestType, null, content.Select(x => new KeyValuePair<string, string>(x.key, x.value)));
+        protected Task<T> SendRequestAsync<T>(string requestType, HttpMethod httpMethod, params (string key, string value)[] content) => SendRequestAsync<T>(requestType, httpMethod, content.Select(x => new KeyValuePair<string, string>(x.key, x.value)));
         #endregion
     }
 }
